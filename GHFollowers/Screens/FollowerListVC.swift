328b35cadcd0bebe1b63e1ee5be0621030678fca
Sarkhan <sarkhanjafarli12@gmail.com>
2022-09-20 22:47:36 +0400

    Finish loading screen

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -40,9 +40,12 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int){
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) {[weak self] result in
             guard let self = self else { return }
-            
+//            #warning("dismiss loading")
+            self.dismissLoadingView()
+
             switch result{
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
