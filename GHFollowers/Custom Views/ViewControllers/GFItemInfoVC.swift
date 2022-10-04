39dc28daf6bf646cffe22d1fa2bf957d42b017d7
Sarkhan <sarkhanjafarli12@gmail.com>
2022-10-04 10:20:30 +0400

    Create superclass

--- GHFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
+++ GHFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -18,7 +18,6 @@ class GFItemInfoVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.layer.cornerRadius = 18
         view.backgroundColor = .secondarySystemBackground
         
